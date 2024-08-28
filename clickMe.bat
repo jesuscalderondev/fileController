654c302050535c3be0aa43f56adf69da56078b30
jesuscalderondev <jesusmcalderonv2002@gmail.com>
2024-08-28 11:08:16 -0500

    init program

---
+++ clickMe.bat
@@ -0,0 +1,21 @@
+title Flask Server
+echo "Se esta empezando a ejecutar el servidor"
+
+echo "descargando el entorno virtual"
+python pip install virtualenv
+
+echo "creando el entorno virtual"
+virtualenv env
+
+echo "activando el entorno virtual"
+call .\env\Scripts\activate
+
+echo "instalando las dependencias"
+:: Instalar dependencias (si es necesario)
+pip install -r requirements.txt
+
+echo "ejecutando el programa"
+:: Ejecutar aplicación Python
+python server.py
+
+pause
